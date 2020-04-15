22ce4c1e24473e75aee04f32f5be0dfc82415624
Valentinmf <figueiredovalentin@gmail.com>
2020-04-15 22:20:38 +0200

    lastcommit

---
+++ readme.md
@@ -0,0 +1,4 @@
+<h1>My first app with Twitter API</h1>
+<hr></hr>
+<p>My first app in Ruby with Twitter api</p>
+<p>With this app you can follow people on twitter, sends tweet</p>
---
+++ spec/app_spec.rb
@@ -0,0 +1,8 @@
+require_relative '../lib/app.rb'
+
+describe "shuffle_in_array_and_pick_random_one" do
+  it "should return TRUE when 5 is picked up and sent to twtr hdls" do
+    it expect(shuffle_in_array_and_pick_random_one(5)).to eq(5) do
+    end
+  end
+end
